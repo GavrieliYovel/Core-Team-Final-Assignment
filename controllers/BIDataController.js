431c58d9e67abafe9d21b76a6bd3b0843dac9466
Gilad Schneider <giladschneider66@gmail.com>
2023-01-02 17:52:03 +0200

    BI data

--- controllers/BIDataController.js
+++ controllers/BIDataController.js
@@ -3,33 +3,70 @@ const axios = require('axios');
 
 
 exports.BIDataController = {
-    MRR(req, res) {
+    getMRR(req, res) {
         axios.get(`https://Billing.render.com/MRR/${req.params.month}`)
-            .then(response=>{
+            .then(response => {
                 res.send(response.data)
             })
-            .catch(mock=>{
+            .catch(mock => {
                 res.send(`MRR for the month of ${req.params.month}`)
             })
     },
 
-    ARR(req, res) {
+    getARR(req, res) {
         axios.get(`https://Billing.render.com/ARR/${req.params.year}`)
-            .then(response=>{
+            .then(response => {
                 res.send(response.data)
             })
-            .catch(mock=>{
+            .catch(mock => {
                 res.send(`ARR for the year of ${req.params.year}`)
             })
     },
 
-    payments(req, res) {
+    getTypeOfPayments(req, res) {
         axios.get(`https://Billing.render.com/payments/${req.params.month}`)
-            .then(response=>{
+            .then(response => {
                 res.send(response.data)
             })
-            .catch(mock=>{
-                res.send(`Payments that succeeded/ failed we hat this month ${req.params.month}`)
+            .catch(mock => {
+                res.send(`Payments that succeeded/ failed we had this month ${req.params.month}`)
             })
 
-    };
+    },
+
+
+
+    getMonthlyExperiments(req, res) {
+        axios.get(`https://Growth.render.com/experiments/${req.params.month}`)
+            .then(response => {
+                res.send(response.data)
+            })
+            .catch(mock => {
+                res.send(`experiments running per month ${req.params.month}`)
+            })
+
+    },
+
+
+    getDistributionByDevice(req, res) {
+        axios.get(`https://Growth.render.com/distribution/${req.params.device}`)
+            .then(response => {
+                res.send(response.data)
+            })
+            .catch(mock => {
+                res.send(`distribution by device ${req.params.device}`)
+            })
+
+    },
+    getDistributionByGeo(req, res) {
+        axios.get(`https://Growth.render.com/distribution/${req.params.geo}`)
+            .then(response => {
+                res.send(response.data)
+            })
+            .catch(mock => {
+                res.send(`distribution by geo ${req.params.geo}`)
+            })
+
+    }
+
+}
--- routers/BIDataRouter.js
+++ routers/BIDataRouter.js
@@ -7,17 +7,21 @@ const BIDataRouter = new Router();
 
 //GET
 //Get registered users per day/month
-BIDataRouter.get();
+// BIDataRouter.get();
 
 //Get total credits usage per day/month per user
 
 
 //Billing BI
-BIDataRouter.get("/MRR/:month", BIDataController.MRR );
-BIDataRouter.get("/ARR/:year", BIDataController.ARR );
-
+BIDataRouter.get("/MRR/:month", BIDataController.getMRR );
+BIDataRouter.get("/ARR/:year", BIDataController.getARR );
 // Get number of succeeded/ failed payments we had this month
-BIDataRouter.get("/payments/:month", BIDataController.payments );
+BIDataRouter.get("/payments/:month", BIDataController.getTypeOfPayments );
+
+// Growth BI
+BIDataRouter.get("/experiments/:month", BIDataController.getMonthlyExperiments );
+BIDataRouter.get("/distribution/:device", BIDataController.getDistributionByDevice );
+BIDataRouter.get("/distribution/:geo", BIDataController.getDistributionByGeo );
 
 
 module.exports = { BIDataRouter };

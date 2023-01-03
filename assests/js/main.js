b034a7b81d76f9a2a0cb99d73b8756ddb336a563
waleed <waleedjohjah@gmail.com>
2023-01-04 00:01:30 +0200

    new charts

--- assests/js/main.js
+++ assests/js/main.js
@@ -62,20 +62,67 @@ window.onload = () => {
 			}]
 		}
 	});
-	new Chart(ctx, {
-		type: 'bar',
+	new Chart(bubblechart, {
+		type: 'bubble',
 		data: {
-			datasets: [{
-				yAxisID: 'yAxis'
-			}]
+			labels: "Africa",
+			datasets: [
+				{
+					label: ["China"],
+					backgroundColor: "rgba(255,221,50,0.2)",
+					borderColor: "rgba(255,221,50,1)",
+					data: [{
+						x: 21269017,
+						y: 5.245,
+						r: 15
+					}]
+				}, {
+					label: ["Denmark"],
+					backgroundColor: "rgba(60,186,159,0.2)",
+					borderColor: "rgba(60,186,159,1)",
+					data: [{
+						x: 258702,
+						y: 7.526,
+						r: 10
+					}]
+				}, {
+					label: ["Germany"],
+					backgroundColor: "rgba(0,0,0,0.2)",
+					borderColor: "#000",
+					data: [{
+						x: 3979083,
+						y: 6.994,
+						r: 15
+					}]
+				}, {
+					label: ["Japan"],
+					backgroundColor: "rgba(193,46,12,0.2)",
+					borderColor: "rgba(193,46,12,1)",
+					data: [{
+						x: 4931877,
+						y: 5.921,
+						r: 15
+					}]
+				}
+			]
 		},
 		options: {
-			scales: {
-				xAxis: {
-					// The axis for this scale is determined from the first letter of the id as `'x'`
-					// It is recommended to specify `position` and / or `axis` explicitly.
-					type: 'time',
-				}
+			title: {
+				display: true,
+				text: 'Predicted world population (millions) in 2050'
+			}, scales: {
+				yAxes: [{
+					scaleLabel: {
+						display: true,
+						labelString: "Happiness"
+					}
+				}],
+				xAxes: [{
+					scaleLabel: {
+						display: true,
+						labelString: "GDP (PPP)"
+					}
+				}]
 			}
 		}
 	});
@@ -84,3 +131,4 @@ window.onload = () => {
 
 const bar           = document.getElementById('bar');
 const doughnut      = document.getElementById('doughnut');
+const bubblechart	=document.getElementById("bubble-chart")

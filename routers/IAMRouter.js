b7ce470246d8d2f93ef547488f74da3f4ea4cec2
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2023-01-06 23:08:05 +0200

    IAM auth check

--- routers/IAMRouter.js
+++ routers/IAMRouter.js
@@ -6,9 +6,9 @@ const IAMRouter = new Router();
 //GET
 IAMRouter.post('/login', IAMController.login);
 // IAMRouter.get('/register', IAMController.IAMregister);
-IAMRouter.get('/details', IAMController.getDetails)
-IAMRouter.get('/token', IAMController.getToken)
-IAMRouter.get('/credits', IAMController.getCredit)
-IAMRouter.get('/credits/:credit', IAMController.setCredit)
+IAMRouter.get('/details', IAMController.getDetails);
+IAMRouter.get('/token', IAMController.getToken);
+IAMRouter.get('/credits', IAMController.getCredit);
+IAMRouter.put('/credits/:credit', IAMController.setCredit);
 
 module.exports = { IAMRouter };

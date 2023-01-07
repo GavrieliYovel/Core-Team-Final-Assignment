1fd8b8edf734b1101fe925d9efa82e76edc92abe
GavrieliYovel <100140349+GavrieliYovel@users.noreply.github.com>
2023-01-07 19:09:28 +0200

    Delete experimentRouter.js

--- experimentRouter.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { experimentController } = require('../controllers/experimentController');
-
-const experimentRouter = new Router();
-
-//GET
-experimentRouter.post('/experiment/new', experimentController.createExperiment);
-
-module.exports = { experimentRouter };

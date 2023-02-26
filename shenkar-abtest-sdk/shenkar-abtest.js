453d5fe4d81e2ec38be8935078aafcb8c7a62c15
GavrieliYovel <yovel.gavrieli@stu.shenkar.ac.il>
2023-02-26 19:19:16 +0200

    start of SDK

--- shenkar-abtest-sdk/shenkar-abtest.js
+++ shenkar-abtest-sdk/shenkar-abtest.js
@@ -1,4 +1,25 @@
 const axios = require("axios");
+const requestIp = require('request-ip');
+const geoip = require("geoip-lite");
+const parser = require("ua-parser-js");
+
+
+const getIp = (endUserReq) => {
+    return requestIp.getClientIp(endUserReq)
+}
+
+const getLocationCode = (endUserReq) => {
+    return geoip.lookup(getIp(endUserReq));
+}
+
+const getUserBrowserDevice = (endUserReq) => {
+    const userAgentInfo = parser(endUserReq.headers["user-agent"]);
+    return {
+        browser: userAgentInfo.browser.name,
+        device: userAgentInfo.device.type || "desktop",
+    };
+}
+
 
 class ABTestSDK {
     account_id;
@@ -23,9 +44,11 @@ class ABTestSDK {
          return this.account_id;
      }
 
-    async getVariant(id,attributes){
-        await axios.post("https://core-team-final-assignment-dev.onrender.com/Growth/experiment/"+id, {
-            attributes
+    async getVariant(experimentId, attributes){
+        await axios.post("https://core-team-final-assignment-dev.onrender.com/Growth/experiment/", {
+            experimentId: experimentId,
+            subscription: "pro",
+            ...attributes
         })
             .then(response => {
                 return response.data;
@@ -34,9 +57,11 @@ class ABTestSDK {
                 return "wrong ";
             })
     }
-    async declareGoal(id,attributes){
-        await axios.post("https://core-team-final-assignment-dev.onrender.com/Growth/experiment/goal/"+id, {
-            attributes
+    async reportGoal(experimentId, goalId ,attributes){
+        await axios.post("https://core-team-final-assignment-dev.onrender.com/Growth/experiment/goal/", {
+            experimentId: experimentId,
+            goalId: goalId,
+            ...attributes
         })
             .then(response => {
                 return response.data;
@@ -46,8 +71,15 @@ class ABTestSDK {
             })
     }
 
-    #filterRequest(req){
-
+    filterUserAttributes(endUserReq, endUserCustomAttributes = null) {
+        return {
+            testAttributes: {
+                location: getLocationCode(endUserReq),
+                ...getUserBrowserDevice(endUserReq)
+            },
+            customAttributes: endUserCustomAttributes,
+            uuid:  endUserReq.cookies.uuid || null
+        }
     }
 
 }
